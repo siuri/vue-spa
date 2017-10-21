26de1bd85cc5c73dc11a0be1a7317c446f33755b
Jung Hyun Kim <siurilove@jungs-mbp.attlocal.net>
2017-10-21 17:37:46 -0500

    Vue Component, Vue Files, ES6 Transpiling

---
+++ src/theme/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <nav class="nav has-shadow">
+        <div class="container">
+          <a href="/">
+            <img src="http://bit.ly/vue-img" alt="Vue SPA"/>
+          </a>
+        </div>
+      </nav>
+      <section class="main-section section"></section>
+      <footer class="footer">
+        <div class="container">
+          <div class="content has-text-centered">
+            Follow us on 3
+            <a href="https://twitter.com/siurilove" target="_blank">Twitter</a>
+          </div>
+        </div>
+      </footer>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
