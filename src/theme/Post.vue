ce25e743dae875ad3300fbe7bd485f9c6b35413b
Jung Hyun Kim <siurilove@jungs-mbp.attlocal.net>
2017-10-21 21:39:47 -0500

    slot

--- src/theme/Post.vue
+++ src/theme/Post.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="card">
       <div class="card-content">
-        <h3>{{ post.title }}</h3> {{post.content}}
+        <slot name="title"></slot>
+        <slot name="content"></slot>
       </div>
       <footer class="card-footer">
-        <a :href="post.link" target="_blank" class="card-footer-item">Read More</a>
+        <a :href="link" target="_blank" class="card-footer-item">Read More</a>
       </footer>
     </div>
 </template>
 <script>
   export default {
-    props: ['post']
+    props: ['link']
   }
 </script>
